5afa8a6bc13b2d19539a0ef9d5aa2197e76b523f
chimpy7 <aleksandar199@hotmail.com>
2024-11-29 19:12:14 +0100

    delar moduler i olika javascript filer för att stoppa error

--- js/twoscripts.js
+++ js/twoscripts.js
@@ -4,13 +4,6 @@ const jebotski  = document.querySelectorAll(".HeaderA")
 const modalss = document.querySelector('.openmodal')
 const huvudmodals = document.querySelector('.huvudmodals')
 
-const modalsss = document.querySelector('.opnkal')
-const huvudmodalss = document.querySelector('.dw')
-
-
-const modalone = document.querySelector('.h3mod')
-const huvudmodaltwo= document.querySelector('.h3body')
-
 
 
 maindisplay.addEventListener('click',function(){
@@ -45,37 +38,3 @@ window.addEventListener('click', (event) => {
 
 
 
-  
-modalsss.addEventListener('click',()=>{
-
-    huvudmodalss.showModal();
-
-
-})
-
-
-
-
-window.addEventListener('click', (event) => {
-    if (event.target === huvudmodalss) {
-     huvudmodalss.close();
-    }
-  });
-
-
-
-  modalone.addEventListener('click',()=>{
-
-    huvudmodaltwo.showModal();
-
-
-})
-
-
-
-
-window.addEventListener('click', (event) => {
-    if (event.target === huvudmodaltwo) {
-        huvudmodaltwo.close();
-    }
-  });
