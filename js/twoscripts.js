7047434cedece2d31e4e8f2c800d07c51dda3f30
chimpy7 <aleksandar199@hotmail.com>
2024-11-29 19:03:30 +0100

    innehåller alla bilder samt grund css html samt javascript behöver design

--- js/twoscripts.js
+++ js/twoscripts.js
@@ -1,8 +1,15 @@
 const maindisplay = document.getElementById("Maindisplay")
 const jebotski  = document.querySelectorAll(".HeaderA")
 
+const modalss = document.querySelector('.openmodal')
+const huvudmodals = document.querySelector('.huvudmodals')
+
+const modalsss = document.querySelector('.opnkal')
+const huvudmodalss = document.querySelector('.dw')
 
 
+const modalone = document.querySelector('.h3mod')
+const huvudmodaltwo= document.querySelector('.h3body')
 
 
 
@@ -15,3 +22,60 @@ maindisplay.addEventListener('click',function(){
 
 
 })
+
+
+modalss.addEventListener('click',()=>{
+
+    huvudmodals.showModal();
+
+
+})
+
+
+
+
+window.addEventListener('click', (event) => {
+    if (event.target === huvudmodals) {
+     huvudmodals.close();
+    }
+  });
+
+
+
+
+
+
+  
+modalsss.addEventListener('click',()=>{
+
+    huvudmodalss.showModal();
+
+
+})
+
+
+
+
+window.addEventListener('click', (event) => {
+    if (event.target === huvudmodalss) {
+     huvudmodalss.close();
+    }
+  });
+
+
+
+  modalone.addEventListener('click',()=>{
+
+    huvudmodaltwo.showModal();
+
+
+})
+
+
+
+
+window.addEventListener('click', (event) => {
+    if (event.target === huvudmodaltwo) {
+        huvudmodaltwo.close();
+    }
+  });
